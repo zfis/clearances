82ba7789e80cf0be497014ae63ecc6aa782804fe
ZeroSystems <sales@erpzero.com>
2019-08-23 13:52:57 +0200

    Add files via upload

---
+++ clearances/__init__.py
@@ -0,0 +1,10 @@
+from . import models
+
+
+
+
+
+
+
+
+
---
+++ clearances/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'clearances',
+    'version': '12.0.0.1',
+    'summary': """ """,
+    'Description': """ """,
+    'Category': 'projects',
+    'author': 'Zero Systems',
+    'company': 'Zero for Information Systems',
+    'website': "https://www.erpzero.com",
+    'email': "sales@erpzero.com",
+    'depends': ['base','sale','purchase','account'],
+    'data': ['views/view.xml','views/clearences_report.xml'],
+    'images': ['static/description/icon.png'],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
+
+
