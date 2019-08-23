930b9c9258039cb31846d2a181927c6facabded6
ZeroSystems <sales@erpzero.com>
2019-08-23 15:13:07 +0200

    Update __manifest__.py

--- clearances/__manifest__.py
+++ clearances/__manifest__.py
@@ -1,8 +1,8 @@
 {
-    'name': 'clearances',
+    'name': 'clearances_ee',
     'version': '12.0.0.1',
-    'summary': """ """,
-    'Description': """ """,
+    'summary': """ odoo Contracting and clearences enterprise """,
+    'Description': """this app allows you create clearence order and invoice for customer and subcontractor""",
     'Category': 'projects',
     'author': 'Zero Systems',
     'company': 'Zero for Information Systems',
